8551f58308ae9e09d47496072ff687236e372ec1
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-07-15 08:32:53 +0530

    fourteenth commit

---
+++ database/1_example.sql
@@ -0,0 +1,17 @@
+-- To create database
+CREATE DATABASE guru;
+
+-- to see all databases
+SHOW DATABASES;
+
+-- to show database (USE db_name)
+USE guru;
+
+-- To see tables
+SHOW TABLES;
+
+-- To delete database (Drop Database db_name)
+DROP DATABASE guru;
+
+
+
