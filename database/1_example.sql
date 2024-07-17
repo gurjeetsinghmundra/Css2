58ae26d5b675fb431ecc4ae5d1237fe45877a9d6
gurjeetsinghmundra <singhgurjeet101003@gmail.com>
2024-07-17 09:01:32 +0530

    database 4

--- database/1_example.sql
+++ database/1_example.sql
@@ -98,3 +98,51 @@ SELECT * FROM department;
 
 INSERT INTO department (department_id) VALUES ("D2");
 
+INSERT INTO department VALUES ("D3","CS");
+
+INSERT INTO department (department_name,department_id) VALUES ("AI","D4");
+
+Create TABLE employee(
+employee_id INT,
+employee_name VARCHAR(70),
+employee_age INT
+);
+
+DESC employee;
+
+INSERT INTO employee VALUES ("1","ABC","18");
+INSERT INTO employee (employee_id,employee_name) VALUES ("2","PQR");
+INSERT INTO employee VALUES ("3","XYZ","21");
+
+SELECT * FROM employee;
+
+INSERT INTO employee VALUES
+(4,"MNO",28),
+(5,"EFG",24),
+(6,"IJK",30);
+
+INSERT INTO employee (employee_id,employee_name) VALUES
+(7,"STU"),
+(8,"WXY");
+
+CREATE TABLE course(
+course_id INT,
+course_name VARCHAR(50),
+student_name VARCHAR(50),
+student_fees INT
+);
+
+INSERT INTO course VALUES 
+(1,"Java FSD","Guru",148000),
+(2,"Python FSD","Amit",148000),
+(3,"Java FSD","Joel",150000),
+(4,"Python FSD","Shriya",122000),
+(5,"Java FSD","Rayyan",115000),
+(6,"Java FSD","Netrali",148000);
+
+SELECT * FROM course;
+
+INSERT INTO course (course_id,course_name,student_name) VALUES 
+(7,"Python FSD","Hamza"),
+(8,"Java FSD","Faizan");
+
